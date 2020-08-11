9251baf617a76636067ca415178042d086b10ab9
leoncavalli <50718317+leoncavalli@users.noreply.github.com>
2020-08-11 12:43:26 +0300

    Product add feauture added.

---
+++ src/app/admin-products/admin-products.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductRepository } from '../repository.model';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'admin-products',
+  templateUrl: './admin-products.component.html',
+  styleUrls: ['./admin-products.component.css']
+})
+export class AdminProductsComponent {
+
+
+  products;
+  model:ProductRepository;
+  selectedProduct:Product;
+
+  constructor() { 
+    this.model=new ProductRepository();
+    this.products=this.model.getProducts();
+  }
+
+  getSelectedProduct(product:Product):boolean{
+    return product==this.selectedProduct;
+  }
+
+  editProduct(product:Product){
+    this.selectedProduct=product;
+  }
+  SaveChanges(){
+    let prod=this.model.getProductsbyId(this.selectedProduct.id);
+    prod.name=this.selectedProduct.name;
+    prod.description=this.selectedProduct.description;
+    prod.price=this.selectedProduct.price;
+  }
+
+  newProduct:Product=new Product();
+
+  get jsonProduct(){
+    return JSON.stringify(this.newProduct);
+  }
+
+  addProduct(p:Product){
+    this.model.addProduct(p);
+  }
+
+
+}
--- src/app/datasource.model.ts
+++ src/app/datasource.model.ts
@@ -7,8 +7,8 @@ export class SimpleDataSource{
             new Product(1,"Samsung","Nice","1.jpg",1000),
             new Product(2,"Oppo","Nice","2.jpg",2000),
             new Product(3,"LG","Nice","3.jpg",3000),
-            new Product(4,"Apple","Nice","4.jpg",4000),
-            new Product(5,"Xiaomi","Nice","5.jpg",5000),
+            new Product(4,"Apple","Nice","1.jpg",4000),
+            new Product(5,"Xiaomi","Nice","2.jpg",5000),
 
         );
     }
