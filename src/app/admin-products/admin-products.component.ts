2b99ddf22227301d510f94a573aea23a75422fc5
leoncavalli <50718317+leoncavalli@users.noreply.github.com>
2020-08-13 10:36:17 +0300

    Cleared admin-products page for new Reactive forms

--- src/app/admin-products/admin-products.component.ts
+++ src/app/admin-products/admin-products.component.ts
@@ -19,29 +19,7 @@ export class AdminProductsComponent {
     this.products=this.model.getProducts();
   }
 
-  getSelectedProduct(product:Product):boolean{
-    return product==this.selectedProduct;
-  }
-
-  editProduct(product:Product){
-    this.selectedProduct=product;
-  }
-  SaveChanges(){
-    let prod=this.model.getProductsbyId(this.selectedProduct.id);
-    prod.name=this.selectedProduct.name;
-    prod.description=this.selectedProduct.description;
-    prod.price=this.selectedProduct.price;
-  }
-
-  newProduct:Product=new Product();
-
-  get jsonProduct(){
-    return JSON.stringify(this.newProduct);
-  }
-
-  addProduct(p:Product){
-    this.model.addProduct(p);
-  }
+  
 
 
 }
