e8f27b4f6aa7d9fc5af215fb175bd74035d7a91a
leoncavalli <50718317+leoncavalli@users.noreply.github.com>
2020-08-16 01:04:47 +0300

    Deleted components for new lesson.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ProductComponent } from './product/product.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {SummaryPipe} from './summary.pipe';
-import { InputEmailDirective } from './input-email.directive';
-import { AdminProductsComponent } from './admin-products/admin-products.component';
+
 @NgModule({
   declarations: [
-  ProductComponent,
-  SummaryPipe,
-  InputEmailDirective,
-  AdminProductsComponent
 ],
   imports: [
     BrowserModule,
@@ -18,6 +11,6 @@ import { AdminProductsComponent } from './admin-products/admin-products.componen
     ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [ProductComponent]
+  bootstrap: []
 })
 export class AppModule { }
--- src/app/product.model.ts
+++
@@ -1,9 +0,0 @@
-export class Product{
-    constructor( 
-        public id?:number,
-        public name?:string,
-        public description?:string,
-        public imgUrl?:string,
-        public price?:number
-        ) {}
-}
