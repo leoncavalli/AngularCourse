37f2c090b9951bf7a5f916d719dfb522bffdeaed
leoncavalli <50718317+leoncavalli@users.noreply.github.com>
2020-08-19 19:57:09 +0300

    Cleared and deleted pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { PostsComponent } from './posts/posts.component';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent],
+    ],
   imports: [
     BrowserModule,
     HttpClientModule
